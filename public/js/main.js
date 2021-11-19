ac8b5d996fe3a62d44b026f810c5eee01be4af17
Syafwan000 <muhammadsyafwan04@gmail.com>
2021-11-19 09:45:56 +0700

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,21 @@
 const popcat = document.getElementById('popcat');
 const counter = document.getElementById('counter');
+const showGIF = document.getElementById('showGIF');
+const closeGIF = document.getElementById('closeGIF');
 var i = 1;
 
 popcat.addEventListener('click', function() {
     counter.value = i++;
 
     const sound = new Audio('public/sound/safensound.mp3');
-    if(counter.value >= 1000 && counter.value < 1001) {
+    if(counter.value >= 500 && counter.value < 501) {
+        showGIF.classList.remove('hidden');
         sound.play();
     }
+});
+
+closeGIF.addEventListener('click', function() {
+    showGIF.classList.add('hidden');
 });
 
 function openMouth() {
